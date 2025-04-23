bcd5084fc4723b3d196134049c0f5aea3d48dd05
graciella-m <graciellamaiamachado@gmail.com>
2025-04-23 11:26:07 -0300

    Criei o projeto; Importei para IDE e iniciei a estrutura dos packages.

---
+++ src/main/java/br/kge/eti/OSApiApplication/api/controller/ClienteController.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.kge.eti.OSApiApplication.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author sesideva
+ */
+@RestController
+public class ClienteController {
+
+    @GetMapping("/clientes")
+    public String listas() {
+        return "teste api";
+    }
+
+}
+
